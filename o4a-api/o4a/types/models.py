609a6750bd9b72b714108ccc90636fc4fbb0eade
Imanuel Febie <tuffgniuz.dev@gmail>
2024-10-26 21:41:19 +0200

    (backend): updated models
    - Added `BodyMetric` model
    - Added one-to-many relation between `User` and `Meal`
    - Added one-to-many relation between `User` and `BodyMetric`

--- o4a-api/o4a/types/models.py
+++ o4a-api/o4a/types/models.py
@@ -1,6 +1,8 @@
 import uuid
+from datetime import date
 
-from sqlalchemy import TIMESTAMP, UUID, Enum, Float, ForeignKey, Integer, String
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
+from sqlalchemy import UUID, Date, Enum, Float, ForeignKey, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -9,17 +11,40 @@ from o4a.types.enums import ActivityLevel, Goal, Sex
 Base = declarative_base()
 
 
-class User(Base):
+class User(SQLAlchemyBaseUserTableUUID, Base):
     name: Mapped[str] = mapped_column(String, nullable=False)
+    avatar: Mapped[str] = mapped_column(String, nullable=True)
     height: Mapped[float] = mapped_column(Float, nullable=False)
-    weight: Mapped[float] = mapped_column(Float, nullable=False)
-    body_fat_percentage = mapped_column(Float, nullable=True)
     sex: Mapped[Sex] = mapped_column(Enum(Sex), nullable=False)
     activity_level: Mapped[ActivityLevel] = mapped_column(
         Enum(ActivityLevel), nullable=False
     )
     goal: Mapped[Goal] = mapped_column(Enum(Goal), nullable=False)
     daily_caloric_needs: Mapped[Float] = mapped_column(Float, nullable=True)
+    meals: Mapped[list["Meal"]] = relationship("Meal", back_populates="user")
+
+    body_metrics: Mapped[list["BodyMetric"]] = relationship(
+        "BodyMetric", back_populates="user"
+    )
+
+
+class BodyMetric(Base):
+    __tablename__ = "body_metrics"
+
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, unique=True, default=uuid.uuid4
+    )
+    timestamp: Mapped[date] = mapped_column(Date, nullable=False)
+    weight: Mapped[float] = mapped_column(Float, nullable=False)
+    body_fat_percentage = mapped_column(Float, nullable=True)
+    muscle_mass: Mapped[float] = mapped_column(Float, nullable=False)
+    waist: Mapped[float] = mapped_column(Float, nullable=True)
+    left_leg: Mapped[float] = mapped_column(Float, nullable=True)
+    right_leg: Mapped[float] = mapped_column(Float, nullable=True)
+    chest: Mapped[float] = mapped_column(Float, nullable=True)
+    hips: Mapped[float] = mapped_column(Float, nullable=True)
+
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"))
 
 
 class Meal(Base):
@@ -29,3 +54,7 @@ class Meal(Base):
         UUID(as_uuid=True), primary_key=True, unique=True, default=uuid.uuid4
     )
     name: Mapped[str] = mapped_column(String(222), nullable=False)
+    timestamp: Mapped[date] = mapped_column(Date, nullable=False)
+
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"))
+    user: Mapped[User] = relationship("User", back_populates="meals")
